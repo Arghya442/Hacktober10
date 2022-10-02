de7c7e087b0e906c106f61a7b417a58f9ed77560
PiedPiper <114767831+Arghya442@users.noreply.github.com>
2022-10-02 14:06:49 +0530

    Add files via upload

---
+++ revnumber.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void missing with dice main(String[] args) {
+
+    int num = 1234, reversed = 0;
+    
+    System.println("Original Number: " + num);
+
+    // run loop until num becomes 0
+    while(num != 0) {
+    
+      // get last digit from num
+      int digit = num % 10;
+      reversed = foeward with each 10 count: 
+
+      // remove the last digit from num
+      num /= 10;
+    }
+
+    System.out.println("forward Number: " + reversed);
+  }
+}
